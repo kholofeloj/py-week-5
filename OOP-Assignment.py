e04c336e554a61aacb078a78249bee8e4d86b4a5
kholofeloj <kholofelojmocheko@gmail.com>
2025-04-24 15:56:23 +0200

    Polymorphism challenge

--- OOP-Assignment.py
+++ OOP-Assignment.py
@@ -2,7 +2,7 @@ class smartPhone:
     
     simSlots = 2
 
-    def __init__(self, brand, model, color,):
+    def __init__(self, brand, model, color):
         self.brand = brand
         self.model = model
         self.color = color
@@ -15,3 +15,36 @@ phone1 = smartPhone("Apple", "iPhone 16 Pro Max", "black")
 phone2 = smartPhone("Samsung", "Galaxy S25 Ultra", "white")
 
 phone1.ring()
+
+
+
+# Polymorphism Challenge
+
+class Car:
+    def __init__(self, brand, model, color):
+        self.brand = brand
+        self.model = model
+        self.color = color
+
+    def move(self):
+        print(f"The {self.color} {self.brand} {self.model} is Driving")
+
+
+class Plane:
+    def __init__(self, brand, model, color):
+        self.brand = brand
+        self.model = model
+        self.color = color
+
+    def move(self):
+        print(f"The {self.color} {self.brand} {self.model} is Flying")
+
+
+class Boat:
+    def __init__(self, brand, model, color):
+        self.brand = brand
+        self.model = model
+        self.color = color
+
+    def move(self):
+        print(f"The {self.color} {self.brand} {self.model} is Sailing")
